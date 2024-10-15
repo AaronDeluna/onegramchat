64e4c88bd250af3275f7168775bf980217d232c4
AaronDeluna <aaron_deluna@mail.ru>
2024-10-15 15:36:00 +0300

    Написал документацию в классе MessageDataValidation к методам messageInputValidation и userExistValidation

--- src/main/java/com/javaacademy/onegramchat/validation/MessageDataValidation.java
+++ src/main/java/com/javaacademy/onegramchat/validation/MessageDataValidation.java
@@ -3,12 +3,18 @@ package com.javaacademy.onegramchat.validation;
 import com.javaacademy.onegramchat.entity.User;
 import com.javaacademy.onegramchat.exceptions.MessageInputException;
 import com.javaacademy.onegramchat.exceptions.UserNotFoundException;
-import com.javaacademy.onegramchat.exceptions.UserRegistrationException;
 
 import java.util.Map;
 
 public class MessageDataValidation {
 
+    /**
+     * Проверяет корректность ввода данных для сообщения.
+     *
+     * @param recipientName имя получателя сообщения.
+     * @param messageText текст сообщения.
+     * @throws MessageInputException если имя получателя или текст сообщения пустые.
+     */
     public static void massageInputValidation(String recipientName, String messageText) throws MessageInputException {
         if (recipientName.trim().isEmpty()) {
             throw new MessageInputException("Имя получателя не может быть пустым, попробуйте еще раз!");
@@ -18,6 +24,13 @@ public class MessageDataValidation {
         }
     }
 
+    /**
+     * Проверяет, существует ли пользователь с заданным именем в системе.
+     *
+     * @param name имя пользователя, которого необходимо проверить.
+     * @param users карта, содержащая всех пользователей, где ключом является имя пользователя.
+     * @throws UserNotFoundException если пользователь с указанным именем не найден.
+     */
     public static void userExistValidation(String name, Map<String, User> users)
             throws UserNotFoundException {
         if (users.containsKey(name)) {
